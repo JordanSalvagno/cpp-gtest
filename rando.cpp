13747c93c76d3e4c15738da9cb50e176b330851d
JordanSalvagno <jsalvagno@mail.csuchico.edu>
2015-09-21 12:46:41 -0700

    modified isDivisibleBy to return false if zero is used

--- rando.cpp
+++ rando.cpp
@@ -4,48 +4,52 @@
 
 /**
  * Worry if only one child is smiling or if all children are smiling
-**/
+ **/
 bool Rando::shouldWorry(bool childASmile, bool childBSmile, bool childCSmile)
 {
-	if( childASmile && childBSmile && childCSmile )
-		return true;
-	else if( childASmile ^ childBSmile ^ childCSmile )
-		return true;
-	else
-		return false;
+  if( childASmile && childBSmile && childCSmile )
+    return true;
+  else if( childASmile ^ childBSmile ^ childCSmile )
+    return true;
+  else
+    return false;
 }
 /**
  * Determines if first or second are evenly divisible by the other.
-**/
+ **/
 bool Rando::isDivisbleBy(int first, int second)
 {
+  if(first == 0 || second == 0)
+  {
+    return false;
+  }
   if(first > second)
   {
-  if((first % second) == 0)
-    return true;
+    if((first % second) == 0)
+      return true;
   }
   else
     if((second % first) == 0)
       return true;
 
-	return false;
+  return false;
 }
 
 /**
  * Given a number, num, says whether or not that number is prime
  * (that is, only evenly divisible by itself and 1. For the sake
  * of this function, do NOT consider 0 prime)
-**/
+ **/
 bool Rando::isPrime(int num)
 {
-	return true;
+  return true;
 }
 
 /**
  * Given two numbers, first and second, return the value of the one
  * that is closest to zero without actually being zero.
-**/
+ **/
 int Rando::nearestToZero(int a, int b)
 {
-	return 0;
+  return 0;
 }
